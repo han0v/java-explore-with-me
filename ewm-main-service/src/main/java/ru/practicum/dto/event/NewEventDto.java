37d4b711beab098d82d0913b2160499a37f21374
faiz <hanovfaiz@yandex.ru>
2025-04-19 15:08:37 +0500

    base first reviu

--- ewm-main-service/src/main/java/ru/practicum/dto/event/NewEventDto.java
+++ ewm-main-service/src/main/java/ru/practicum/dto/event/NewEventDto.java
@@ -13,6 +13,7 @@ import ru.practicum.model.Location;
 import java.time.LocalDateTime;
 
 @Data
+@Builder(builderClassName = "NewEventDtoBuilder")
 @NoArgsConstructor
 @AllArgsConstructor
 @Validated
