7b5d436e300d557cd888e8244af8cf6a9a6fdfd0
faiz <hanovfaiz@yandex.ru>
2025-04-19 20:28:38 +0500

    base first reviu

--- ewm-stat-service/ewm-stat-server/src/main/java/ru/practicum/stats/controller/StatsController.java
+++ ewm-stat-service/ewm-stat-server/src/main/java/ru/practicum/stats/controller/StatsController.java
@@ -2,7 +2,6 @@ package ru.practicum.stats.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
